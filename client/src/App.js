a1646567d87474aa44632609ead717ede39ed0fa
MIR HB RAHMAN <ahadcr0@gmail.com>
2022-07-22 22:12:41 +0600

    Create post form client

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
+import PostCreate from "./PostCreate";
+
 function App() {
   return (
-    <div>Blog app</div>
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate />
+    </div>
   );
 }
 
