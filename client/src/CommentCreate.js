d2562a1bda6283f9464efaa910dc8bc66b4d9f60
MIR HB RAHMAN <ahadcr0@gmail.com>
2022-07-23 21:39:37 +0600

    Created comment

--- client/src/CommentCreate.js
+++ client/src/CommentCreate.js
@@ -1,13 +1,19 @@
 import React, {useState} from "react";
+import axios from 'axios';
 
 export default ({postId}) => {
 
     const [content, setContent] = useState('');
 
-    const onSubmit = (event)=>{
+    const onSubmit = async(event)=>{
         event.preventDefault();
         
-    }}
+        await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+            content
+        })
+
+        setContent('')
+    }
 
     return <div>
         <form onSubmit={onSubmit}>
--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,5 +1,6 @@
     import React, { useState, useEffect} from "react";
     import axios from 'axios';
+    import CommentCreate from "./CommentCreate";
 
     export default () => {
 
@@ -24,6 +25,7 @@
                 >
                     <div className="card-body">
                         <h3>{post.title}</h3>
+                        <CommentCreate postId={post.id} />
                     </div>
                 </div>
             )
