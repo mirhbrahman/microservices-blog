9cd51a07020abd58f60c39cae0425bc9f3ae2680
MIR HB RAHMAN <ahadcr0@gmail.com>
2022-07-25 23:45:16 +0600

    Added comment moderation service

---
+++ moderation/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/events', (req, res)=>{
+    
+});
+
+app.listen(4003, ()=>{
+    console.log('Listing on port 4003')
+});
