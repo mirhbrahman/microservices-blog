f0923e947ea70663eb64a9bce5bd26861b6ea9d4
MIR HB RAHMAN <ahadcr0@gmail.com>
2022-07-26 23:42:58 +0600

    Implement event sync

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,13 +9,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res)=>{
-    res.send(posts);
-});
-
-app.post('/events', (req, res)=>{
-    const { type, data } = req.body;
-
+const handleEvents = (type, data)=>{
     if(type === 'PostCreated'){
         const {id, title} = data;
 
@@ -38,13 +33,30 @@ app.post('/events', (req, res)=>{
         comment.content = content;
 
     }
+}
 
-    console.log(posts);
+app.get('/posts', (req, res)=>{
+    res.send(posts);
+});
+
+app.post('/events', (req, res)=>{
+    const { type, data } = req.body;
+
+    handleEvents(type, data);
 
     res.send({});
     
 });
 
-app.listen(4002, ()=>{
-    console.log('Listing on port 4002')
+app.listen(4002, async()=>{
+    console.log('Listing on port 4002');
+
+    const res = await axios.get('http://localhost:4005/events');
+
+    for(let event of res.data){
+        console.log('Precession event: ', event.type);
+
+        handleEvents(event.type, event.data);
+    }
+
 });
