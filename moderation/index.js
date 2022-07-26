f0eb46e5b5f5c6ad49a76016dcfa5cc2df41e399
MIR HB RAHMAN <ahadcr0@gmail.com>
2022-07-26 22:57:04 +0600

    CommentModerated and CommentUpdated event create

--- moderation/index.js
+++ moderation/index.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { default: axios } = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/events', (req, res)=>{
-    
+app.post('/events', async (req, res)=>{
+    const {type, data} = req.body;
+
+    if(type === 'CommentCreated'){
+        const status = data.content.includes('orange') ? 'rejected' : 'approved';
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentModerated',
+            data: {
+                id: data.id,
+                postId: data.postId,
+                status,
+                content: data.content
+            }
+        });
+    }
+    res.send({});
 });
 
 app.listen(4003, ()=>{
